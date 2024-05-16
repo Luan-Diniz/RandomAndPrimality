23fabe71d6cb30e7a7840c517174fff6fcbad00d
Luan-Diniz <luan.diniz.moraes@hotmail.com>
2024-05-16 17:53:15 -0300

    Implementado Miller Rabin

--- VerifyPrimality/miller_rabin.py
+++ VerifyPrimality/miller_rabin.py
@@ -1,3 +1,36 @@
+import random
+
 class MillerRabin:
-    def __init__(self) -> None:
-        pass
+
+    @staticmethod
+    def testPrimality(n: int) -> str:
+        assert (n < 3 or n % 2 != 0), f"Input at MillerRabin.testPrimality(1) should be odd and greater than 3. Failed value: {n}."
+        k, m = MillerRabin.__findKandM(n)
+        a = random.randint(1, n-1)
+        b = (a ** m) % n             
+
+        if b % n == 1:
+            return "Probably prime"
+        for _ in range(0, k):    # Repeats k times.
+            if (b % n == n-1):
+                return "Probably prime"
+            else:
+                b = b**2 % n
+        return "Composite"
+
+
+    @staticmethod
+    def __findKandM(n: int) -> tuple[int, int]:
+        k = 0
+        m = 0
+        n = n-1
+
+        while True:
+            if (n % 2 == 0):
+                k += 1
+                n /= 2
+            else:
+                m = n
+                break
+        
+        return (k, int(m))
