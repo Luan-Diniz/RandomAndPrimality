e249539b313f4a2027d001a0ab44d82e7935781e
Luan-Diniz <luan.diniz.moraes@hotmail.com>
2024-05-16 13:10:27 -0300

    começo implementação

--- PseudoRandomNumberGenerator/linear_congruential_generator.py
+++ PseudoRandomNumberGenerator/linear_congruential_generator.py
@@ -1,3 +1,20 @@
 class LinearCongruentialGenerator:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, Xo, m, a, c) -> None:
+        self.__Xn = Xo
+        self.__m = m
+        self.__a = a
+        self.__c = c
+
+    def generateNumber(self):
+        self.__Xn = (self.__a * self.__Xn + self.__c) % self.__m
+
+        return self.__Xn
+
+
+    def generateGenerator(self, number):
+        count = 0
+        while count < number:
+            count += 1
+            self.__Xn = (self.__a * self.__Xn + self.__c) % self.__m
+
+            yield self.__Xn
--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from PseudoRandomNumberGenerator import *
 if __name__ == '__main__':
     print("Hello World")   # Run tests.
 
+    lcg = LinearCongruentialGenerator(3,9,7,4)  #Xo = 3, m = 9, a = 7 , c = 4
 
+
+    for i in lcg.generateGenerator(9):      # Exemplo do slide
+        print(i)
 
 
 
