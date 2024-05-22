4e5eaf487cb17dd739c022e13cfcb9d60a667c0a
Luan-Diniz <luan.diniz.moraes@hotmail.com>
2024-05-22 14:58:23 -0300

    Mede o tempo, muda o algoritmo para achar o primo.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from utils import *
 from random import randint
+from datetime import datetime
 
 if __name__ == '__main__':
     #sizes = [40, 56, 80, 128, 168, 224, 256, 512, 1024, 2048, 4096]
@@ -10,9 +11,16 @@ if __name__ == '__main__':
     # Measure of execution time to create the random numbers and test its primality.
          
     for size in sizes:
+        #lcg = LinearCongruentialGenerator(randint(0, 1000000) ,2**32,1664525,1013904223)   # m = 2**32, a = 1664525, c = 1013904223 From Wikipedia
+        #number = RandomNumberWithSpecificSize(size, 32, lcg)
+        #print(number)
+        #print("PRIME: ", end = "")
+        #print(FindPrimeNumberWithSpecificSize(size, number, MillerRabin.testPrimality))
+        #print()
+        
         lcg = LinearCongruentialGenerator(randint(0, 1000000) ,2**32,1664525,1013904223)   # m = 2**32, a = 1664525, c = 1013904223 From Wikipedia
-        number = RandomNumberWithSpecificSize(size, 32, lcg)
-        print(number)
-        print("PRIME: ", end = "")
-        print(FindPrimeNumberWithSpecificSize(size, number, MillerRabin.testPrimality))
-        print()
+        start_time = datetime.now()
+        ret = FindPrimeNumberWithSpecificSize(size, MillerRabin.testPrimality, 32, lcg)
+        end_time = datetime.now()
+        print(f"Time: {end_time - start_time}\nPRIME: {ret}")
+             
--- utils.py
+++ utils.py
@@ -2,10 +2,20 @@ from collections.abc import Callable
 from crypto_library.PseudoRandomNumberGenerator import *
 from crypto_library.VerifyPrimality import *
 
+first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                     31, 37, 41, 43, 47, 53, 59, 61, 67,
+                     71, 73, 79, 83, 89, 97, 101, 103,
+                     107, 109, 113, 127, 131, 137, 139,
+                     149, 151, 157, 163, 167, 173, 179,
+                     181, 191, 193, 197, 199, 211, 223,
+                     227, 229, 233, 239, 241, 251, 257,
+                     263, 269, 271, 277, 281, 283, 293,
+                     307, 311, 313, 317, 331, 337, 347, 349]
+
 def RandomNumberWithSpecificSize(expected_output_size: int, size_output_number_generator: int, number_generator: RandomNumberGenerator) -> int:
 
-    remaining_bits = expected_output_size
-    output_number = '0b'
+    remaining_bits = expected_output_size - 1    # MSB is 1, that's why -1 in this line.
+    output_number = '0b1'
 
     while remaining_bits != 0:
         random_number = bin(number_generator.generateNumber())[2:] # Strip off '0b'
@@ -22,24 +32,22 @@ def RandomNumberWithSpecificSize(expected_output_size: int, size_output_number_g
 
     return int(output_number, 2)
 
+def RandomOddNumberWithSpecificSize(expected_output_size: int, size_output_number_generator: int, number_generator: RandomNumberGenerator) -> int:
+    # expected_output_size - 1 because the LSB will be 1.
+    number = RandomNumberWithSpecificSize(expected_output_size - 1, size_output_number_generator, number_generator)
 
-def FindPrimeNumberWithSpecificSize(number_size: int, generated_number: int, used_function: Callable[[None], str], number_rounds: int = 5) -> int:
+    return int(bin(number) + '1',2)  # LSB is 1, so the number is odd
+
+def FindPrimeNumberWithSpecificSize(number_size: int, 
+                                    used_function: Callable[[None], str], size_output_number_generator: int,
+                                    number_generator: RandomNumberGenerator, number_rounds: int = 5) -> int:
     # used_function is instance of PrimalityTester
-    #TODO: ignore test of numbers that end in 5?
-    #TODO: parallel programming?
-    if (generated_number % 2 == 0):
-        generated_number += 1
-    original_number = generated_number
-    maybe_prime_number = generated_number
-    offset = 2
 
     while True:
-        if (maybe_prime_number.bit_count() > number_size):
-            offset = -2
-            maybe_prime_number = original_number + offset
-
-        if (used_function(maybe_prime_number, number_rounds) == "Probably prime"):
-            #TODO: test if it is prime indeed with root of n
-            return maybe_prime_number
-        maybe_prime_number += offset
-        print(f"TESTANDO: {maybe_prime_number}")
+        random_number = RandomOddNumberWithSpecificSize(number_size, size_output_number_generator, number_generator)
+        for prime in first_primes_list:
+            if (random_number % prime == 0):
+                continue
+        if (used_function(random_number, number_rounds) == "Probably prime"):
+            return random_number
+            
