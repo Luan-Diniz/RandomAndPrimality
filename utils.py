14cef810447dd109bed18bd45fdaaf135772b595
Luan-Diniz <luan.diniz.moraes@hotmail.com>
2024-05-22 15:26:38 -0300

    Atualiza, muito tempo sendo usado. revisar o codigo com alguma referencia eh necessário

--- utils.py
+++ utils.py
@@ -12,6 +12,31 @@ first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
                      263, 269, 271, 277, 281, 283, 293,
                      307, 311, 313, 317, 331, 337, 347, 349]
 
+
+def FindPrimeNumberWithSpecificSize(number_size: int, 
+                                    used_function: Callable[[None], str], size_output_number_generator: int,
+                                    number_generator: RandomNumberGenerator, number_rounds: int = 5) -> int:
+    # used_function is instance of PrimalityTester
+
+    while True:
+        #random_number = RandomOddNumberWithSpecificSize(number_size, size_output_number_generator, number_generator)
+        random_number = randint(2**(number_size-1)+1, 2**number_size-1)
+        if (random_number % 2 == 0):
+            random_number += 1
+        for prime in first_primes_list:
+            if (random_number % prime == 0):
+                continue
+        if (used_function(random_number, number_rounds) == "Probably prime"):
+            return random_number
+            
+
+def RandomOddNumberWithSpecificSize(expected_output_size: int, size_output_number_generator: int, number_generator: RandomNumberGenerator) -> int:
+    # expected_output_size - 1 because the LSB will be 1.
+    number = RandomNumberWithSpecificSize(expected_output_size - 1, size_output_number_generator, number_generator)
+
+    return int(bin(number) + '1',2)  # LSB is 1, so the number is odd
+
+
 def RandomNumberWithSpecificSize(expected_output_size: int, size_output_number_generator: int, number_generator: RandomNumberGenerator) -> int:
 
     remaining_bits = expected_output_size - 1    # MSB is 1, that's why -1 in this line.
@@ -31,23 +56,3 @@ def RandomNumberWithSpecificSize(expected_output_size: int, size_output_number_g
             output_number += random_number
 
     return int(output_number, 2)
-
-def RandomOddNumberWithSpecificSize(expected_output_size: int, size_output_number_generator: int, number_generator: RandomNumberGenerator) -> int:
-    # expected_output_size - 1 because the LSB will be 1.
-    number = RandomNumberWithSpecificSize(expected_output_size - 1, size_output_number_generator, number_generator)
-
-    return int(bin(number) + '1',2)  # LSB is 1, so the number is odd
-
-def FindPrimeNumberWithSpecificSize(number_size: int, 
-                                    used_function: Callable[[None], str], size_output_number_generator: int,
-                                    number_generator: RandomNumberGenerator, number_rounds: int = 5) -> int:
-    # used_function is instance of PrimalityTester
-
-    while True:
-        random_number = RandomOddNumberWithSpecificSize(number_size, size_output_number_generator, number_generator)
-        for prime in first_primes_list:
-            if (random_number % prime == 0):
-                continue
-        if (used_function(random_number, number_rounds) == "Probably prime"):
-            return random_number
-            
